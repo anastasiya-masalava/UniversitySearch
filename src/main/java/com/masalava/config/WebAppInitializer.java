f08084dbd7f7d38e95008e629a062dd08136dfcd
Nastushka_zzz <66745124+anastasiamasalova2020@users.noreply.github.com>
2021-02-05 17:18:52 +0300

    Small updates

--- src/main/java/com/masalava/config/WebAppInitializer.java
+++ src/main/java/com/masalava/config/WebAppInitializer.java
@@ -1,6 +1,5 @@
 package com.masalava.config;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -17,9 +16,7 @@ public class WebAppInitializer implements WebApplicationInitializer {
         AnnotationConfigWebApplicationContext context=new AnnotationConfigWebApplicationContext();
         context.register(SpringConfig.class, WebConfig.class);
         context.setServletContext(servletContext);
-
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
-
         dispatcher.setLoadOnStartup(1);
         dispatcher.addMapping("/");
 
